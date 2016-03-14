d284a7739e2a647e793397c1b2513e3f0dc93136
erich brunner <erich.brunner@live.at>
2016-03-14 10:43:55 +0100

    added Database Project

---
+++ Database.SSDT/Person.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[Person]
+(
+	[Id] INT NOT NULL PRIMARY KEY IDENTITY, 
+    [Name] NVARCHAR(150) NOT NULL, 
+    [Address] NVARCHAR(150) NOT NULL, 
+    [DateOfBirth] DATETIME NOT NULL, 
+    [Notes] NVARCHAR(MAX) NULL, 
+    [HobbiesId] INT NULL, 
+    CONSTRAINT [FK_Person_ToHobbies] FOREIGN KEY ([HobbiesId]) REFERENCES [Hobbies]([Id]) 
+)
